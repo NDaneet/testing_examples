1da87f09b14189851013835382df93c965a82202
Daniel Tobi <danielnyorere@gmail.com>
2023-06-19 03:08:27 +0100

    Modified the main.dart

--- state_management_with_provider/lib/main.dart
+++ state_management_with_provider/lib/main.dart
@@ -5,12 +5,7 @@ import "screens/favorites_screen.dart";
 import "screens/fruits_screen.dart";
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => FavouriteModel(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,17 +13,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (context) => FavouriteModel(),
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.red,
           useMaterial3: true,
         ),
         initialRoute: "/",
-        routes: <String, WidgetBuilder> {
-          "/" : (context) => const FruitScreen(),
-          "/favourite_screen" : (context) =>  const FavouriteScreen()
+        routes: <String, WidgetBuilder>{
+          "/": (context) => const FruitScreen(),
+          "/favourite_screen": (context) => const FavouriteScreen()
         },
+      ),
     );
   }
 }
