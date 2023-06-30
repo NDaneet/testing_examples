f7439deabd17384789552ef75e693a7e55dfbeaa
Daniel Tobi <danielnyorere@gmail.com>
2023-06-30 23:45:57 +0100

    Modified the favourites_screen.dart

--- state_management_with_provider/lib/screens/favourites_screen.dart
+++ state_management_with_provider/lib/screens/favourites_screen.dart
@@ -7,12 +7,11 @@ import '../model/fruits_model.dart';
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({super.key});
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          ///removed the context of appbar to see if it will work
           _appBar(),
           SliverList(
             delegate: _buildFavouriteFruitsList(context),
@@ -20,41 +19,40 @@ class FavouriteScreen extends StatelessWidget {
         ],
       ),
     );
+  }
 
-    }
 
-//remove the context
-  SliverAppBar _appBar(){
-  return const SliverAppBar(
-    expandedHeight: 150.0,
-    backgroundColor: Color.fromARGB(255, 218, 157, 153),
-    floating: true,
-    pinned: true,
-    flexibleSpace: FlexibleSpaceBar(
-      expandedTitleScale: 2.5,
-      title: Text(
-        "Favourites",
-        style: TextStyle(
-          color: Colors.black,
+  SliverAppBar _appBar() {
+    return const SliverAppBar(
+      expandedHeight: 150.0,
+      backgroundColor: Color.fromARGB(255, 218, 157, 153),
+      floating: true,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        expandedTitleScale: 2.5,
+        title: Text(
+          "Favourites",
+          style: TextStyle(
+            color: Colors.black,
+          ),
         ),
+        centerTitle: true,
       ),
-      centerTitle: true,
-    ),
-  );
-}
+    );
+  }
 
-  SliverChildDelegate _buildFavouriteFruitsList(BuildContext context){
+  SliverChildDelegate _buildFavouriteFruitsList(BuildContext context) {
     FavouriteModel favouriteList = Provider.of<FavouriteModel>(context);
     final List<FruitListTile> favouriteFruitListTile = [];
-  for (Fruit fruit in favouriteList.favoriteFruits) {
-    favouriteFruitListTile.add(
-      FruitListTile(
+    for (Fruit fruit in favouriteList.favouriteFruits) {
+      favouriteFruitListTile.add(FruitListTile(
         fruitName: fruit.fruitName,
-        fruitAssetLocation: fruit.fruitAssetLocation,));
-  }
+        fruitAssetLocation: fruit.fruitAssetLocation,
+      ));
+    }
     return SliverChildBuilderDelegate(
-    (BuildContext context, int index) => favouriteFruitListTile[index],
-    childCount: favouriteFruitListTile.length,
+      (BuildContext context, int index) => favouriteFruitListTile[index],
+      childCount: favouriteFruitListTile.length,
     );
   }
 }
